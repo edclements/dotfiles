fea0f82682e13176f696c81258766c281821416f
Ed Clements <ed.clements@gmail.com>
2023-05-03 15:37:25 +0100

    check for brew

--- .config/zsh/path.zsh
+++ .config/zsh/path.zsh
@@ -1,6 +1,9 @@
 typeset -U path
 export path=(~/bin $path)
 export path=(.git/safe/../../bin $path)
+
+if which brew > /dev/null; then
 export path=($(brew --prefix coreutils)/libexec/gnubin $path)
+fi
 
 source ~/.cargo/env
